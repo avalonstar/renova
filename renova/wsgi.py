ade8b1d18d40192df3703c5b6c11c1f23822df42
Bryan Veloso <bryan@avalonstar.com>
2018-03-29 10:04:28 -0700

    Changing my mind again.

--- renova/wsgi.py
+++ renova/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for minerva project.
+WSGI config for renova project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "minerva.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "renova.settings")
 
 application = get_wsgi_application()
