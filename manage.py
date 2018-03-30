51153c88cff92bc3f1027f41f6265b91e6b04263
Bryan Veloso <bryan@avalonstar.com>
2018-03-29 22:58:38 -0700

    Fix dotenv.

--- manage.py
+++ manage.py
@@ -2,9 +2,9 @@
 import os
 import sys
 
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 
-load_dotenv()
+load_dotenv(find_dotenv())
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "renova.settings")
--- renova/wsgi.py
+++ renova/wsgi.py
@@ -10,9 +10,9 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 
-load_dotenv()
+load_dotenv(find_dotenv())
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "renova.settings")
 
