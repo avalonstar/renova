a705477d1dd37d5c3d37a14d38ebd7389cb10e36
Bryan Veloso <bryan@avalonstar.com>
2018-03-30 12:53:02 -0700

    Exclude GMs.

--- ragnarok/managers.py
+++ ragnarok/managers.py
@@ -4,4 +4,4 @@ from django.db import models
 class AccountManager(models.Manager):
 
     def get_queryset(self):
-        return super().get_queryset().exclude(account_id=1)
+        return super().get_queryset().exclude(account_id=1).exclude(group_id=99)
