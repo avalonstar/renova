5677cebf5d4193520f50fa08844f09412040a237
Bryan Veloso <bryan@avalonstar.com>
2018-03-30 15:07:13 -0700

    Seeing what the allauth signals give us.

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -1,1 +1,18 @@
 from django.db import models
+from django.dispatch import receiver
+
+from allauth.socialaccount import signals
+
+
+@receiver(signals.social_account_added)
+def added_socialaccount(request, sociallogin, **kwargs):
+    a = {}
+    a.update(**kwargs)
+    return print(request, sociallogin, a)
+
+
+@receiver(signals.social_account_updated)
+def updated_socialaccount(request, sociallogin, **kwargs):
+    a = {}
+    a.update(**kwargs)
+    return print(request, sociallogin, a)
