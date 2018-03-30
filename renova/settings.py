6c14f7c64258fae97e09b488405d47d46c0d4ee8
Bryan Veloso <bryan@avalonstar.com>
2018-03-30 00:12:37 -0700

    12factor SECRET_KEY. (Also regenerated it.)

--- renova/settings.py
+++ renova/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9zc53@j-^n9)n3(@55r(#z+&fv%r0eh4*8+18umiuotdgh(a$s'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
