5e01d343cb702f723ec5ef0746198d8efbdbc2b2
Bryan Veloso <bryan@avalonstar.com>
2018-03-30 00:09:25 -0700

    Adding allauth's authentication backend.

--- renova/settings.py
+++ renova/settings.py
@@ -98,6 +98,11 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
