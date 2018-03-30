2b986620db30ee1f8bf7bd0fff4aa8b9334a2073
Bryan Veloso <bryan@avalonstar.com>
2018-03-30 02:40:42 -0700

    Attempting to only enable social account creation.

---
+++ players/adapters.py
@@ -0,0 +1,14 @@
+from allauth.account.adatper import DefaultAccountAdapter
+from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+
+
+class AccountAdapter(DefaultAccountAdapter):
+
+    def is_open_for_signup(self, request):
+        return False
+
+
+class SocialAccountAdapter(DefaultSocialAccountAdapter):
+
+    def is_open_for_signup(self, request):
+        return True
