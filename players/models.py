0604602ac949f4709afdb87b0a3f02ee624e08db
Bryan Veloso <bryan@avalonstar.com>
2018-03-30 12:34:36 -0700

    Grab a Player's connected accounts.

--- players/models.py
+++ players/models.py
@@ -1,6 +1,14 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from ragnarok.models import Account
+
 
 class Player(AbstractUser):
-    pass
+
+    def __str__(self):
+        return self.username
+
+    @property
+    def accounts(self):
+        return Account.objects.filter(username=self.username)
