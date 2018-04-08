5b2ac1facf4f1891a68d7cd60207fdf2300a1313
Bryan Veloso <bryan@avalonstar.com>
2018-04-08 12:58:31 -0700

    Correlate players with their account ID(s).

--- apps/players/models.py
+++ apps/players/models.py
@@ -1,15 +1,25 @@
 from django.contrib.auth.models import AbstractUser
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 from django.utils.functional import cached_property
 
-from apps.ragnarok.models import Account
+from apps.ragnarok.models import Account, Character, Storage
 
 
 class Player(AbstractUser):
+    account_ids = ArrayField(models.IntegerField(), blank=True, default=list, size=2)
 
     def __str__(self):
         return self.username
 
     @cached_property
     def accounts(self):
-        return Account.objects.filter(username=self.username)
+        return Account.objects.filter(account_id__in=self.account_ids)
+
+    @cached_property
+    def characters(self):
+        return Character.objects.filter(account_id__in=self.account_ids)
+
+    @cached_property
+    def storage(self):
+        return Storage.objects.filter(account_id__in=self.account_ids)
