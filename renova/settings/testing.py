c4cd4e3f5216515512efab31d901cfeef52ba3d3
Bryan Veloso <bryan@avalonstar.com>
2018-04-09 23:22:21 -0700

    Specify TRAVIS_ENVIRONMENT for enviornment changes.

--- renova/settings/testing.py
+++ renova/settings/testing.py
@@ -7,7 +7,16 @@ class Testing(Settings):
     DEBUG = values.BooleanValue(False)
     TEMPLATE_DEBUG = DEBUG
 
-    Settings.DATABASES['default']['NAME'] = 'test_renova'
+    if 'TRAVIS' in os.environ:
+        TRAVIS_ENVIRONMENT = True
+
+    if TRAVIS_ENVIRONMENT:
+        Settings.DATABASES['default'] = {
+            'NAME': 'travisdb', 'USER': 'postgres', 'PASSWORD': '', 'PORT': ''
+        }
+    else:
+        Settings.DATABASES['default']['NAME'] = 'test_renova'
+
     Settings.DATABASES['ragnarok'] = {
         'ENGINE': 'django.db.backends.sqlite3', 'NAME': ':memory:'
     }
