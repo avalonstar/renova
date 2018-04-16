1625e855602f13544c9fd3733ab3cb16fa246500
Bryan Veloso <bryan@avalonstar.com>
2018-04-15 22:31:24 -0700

    Move App to containers.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { Route, Switch } from 'react-router-dom';
+import { ConnectedRouter } from 'react-router-redux';
 
 import { ThemeProvider } from 'styled-components';
 
+import App from 'containers/App';
 import baseStyles, { foundation } from 'helpers/foundation';
 
-import './index.css';
-import App from './App';
+import store, { history } from './store';
 import registerServiceWorker from './registerServiceWorker';
 
 const render = () => {
   baseStyles();
   ReactDOM.render(
-    <ThemeProvider theme={foundation}>
-      <App />
-    </ThemeProvider>,
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <ThemeProvider theme={foundation}>
+          <Switch>
+            <Route component={App} />
+          </Switch>
+        </ThemeProvider>
+      </ConnectedRouter>
+    </Provider>,
     document.getElementById('root')
   );
-}
+};
 
 render();
 registerServiceWorker();
