10c8d246a094d4249d36d1cef5783df34c25c6f9
Bryan Veloso <bryan@avalonstar.com>
2018-04-21 17:38:39 -0700

    Use a singular "item".

--- frontend/src/scenes/Root.js
+++ frontend/src/scenes/Root.js
@@ -31,7 +31,7 @@ const Database = () => (
     <RouteWithLayout
       layout={Dashboard}
       component={Item}
-      path="/database/items/:pk"
+      path="/database/item/:pk"
     />
   </Switch>
 );
